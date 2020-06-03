af4d33c841d7fb09e4f855cc770a46d12309c9e3
hanxinyan <metatron96@126.com>
2020-06-03 17:53:44 +0800

    多机器人协调测试V3，多机器人协调测试全部完成

--- include/define.h
+++ include/define.h
@@ -22,7 +22,7 @@
 
 #define ROBOTNUM 6  // 机器人数量
 #define TASKPOINT 8  // 任务点数量
-#define TASKCAPACITY 150  // 任务点任务容量
+#define TASKCAPACITY 5  // 任务点任务容量
 #define COORDINATE_LENGTH 3  // 协调长度
 #define RAND_NUM rand() / static_cast<double>(RAND_MAX) / 100  // 随机数， 0～0.01
 #define SINGLE_COORDINATE 1  // 单个机器人协调策略
